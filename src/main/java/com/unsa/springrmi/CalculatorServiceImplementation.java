7d5c96c851558dd3c2d3f3487a2225cc174d5bc6
MarcoAntonioPdLA <mponcedeleona@unsa.edu.pe>
2025-05-18 21:45:20 -0500

    Modificando implementación de la interfaz remota para que se trabaje con Spring (no hace falta UnicastRemoteObject)

--- src/main/java/com/unsa/springrmi/CalculatorServiceImplementation.java
+++ src/main/java/com/unsa/springrmi/CalculatorServiceImplementation.java
@@ -1,22 +1,19 @@
 package com.unsa.springrmi;
 
 import java.rmi.RemoteException;
-import java.rmi.server.UnicastRemoteObject;
 
-public class CalculatorServiceImplementation extends UnicastRemoteObject implements CalculatorService {
-	//Constante que sirve de buena práctica para implementar Serializable
-	//de forma implícita con UnicastRemoteObject.
-	private static final long serialVersionUID = 1L;
-	
-	public CalculatorServiceImplementation() throws RemoteException {
-		super();
-	}
-	
-	public int add(int a, int b) throws RemoteException {
-		return a + b;
-	}
-	
-	public int subtract(int a, int b) throws RemoteException {
-		return a - b;
-	}
+public class CalculatorServiceImplementation implements CalculatorService {
+
+    public CalculatorServiceImplementation() throws RemoteException {
+    }
+
+    @Override
+    public int add(int a, int b) throws RemoteException {
+        return a + b;
+    }
+
+    @Override
+    public int subtract(int a, int b) throws RemoteException {
+        return a - b;
+    }
 }
